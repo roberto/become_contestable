3fb8b1953df6b54037d2c62da8a66628ca18e6fa
Roberto Soares <roberto.tech@gmail.com>
2008-12-10 15:56:45 -0200

    become_contest: added dependent => :destroy for associations

--- lib/become_contest.rb
+++ lib/become_contest.rb
@@ -6,10 +6,10 @@ module BecomeContest
 
   module ActMethods
     def become_contest(options = {})
-      has_many :votes, :as => :voteable
-      has_many :nominations, :as => :nominatable
-      has_one :contest_option, :as => :contest
-      has_many :winners, :as => :contest, :class_name => "ContestWinner"
+      has_many :votes, :as => :voteable, :dependent => :destroy
+      has_many :nominations, :as => :nominatable, :dependent => :destroy
+      has_one :contest_option, :as => :contest, :dependent => :destroy
+      has_many :winners, :as => :contest, :class_name => "ContestWinner", :dependent => :destroy
       
       before_save :save_contest_options
       
@@ -27,17 +27,17 @@ module BecomeContest
       def top(length = 10)
         
       end
-    end
-
-    module ClassMethods
-
+      
       def save_contest_options
-        self.build_contest_option(self.class.become_contest_options) if self.contest_options = nil        
+        self.build_contest_option(self.class.become_contest_options) if self.contest_option.nil?    
       end
       
       def contest_option_attributes=(contest_option_attributes)
         self.contest_option = ContestOption.new(contest_option_attributes.reverse_merge(self.class.become_contest_options))
       end
+    end
+
+    module ClassMethods
 
     end
   end
